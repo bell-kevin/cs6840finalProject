a7d2c338791a6cf97270796ddf1fe51775b62326
Kevin Bell <bellKevin@pm.me>
2025-07-30 10:24:00 -0600

    Improve example output

--- example_usage.py
+++ example_usage.py
@@ -32,12 +32,18 @@ def main() -> None:
     mc = CTLModelChecker(ts)
     mc_until = CTLModelChecker(ts_until)
 
-    print(mc.satisfies("EF p"))
-    print(mc.satisfies("AG p"))
-    print(mc.satisfies("AF p"))
-    print(mc.satisfies("EG q"))
-    print(mc_until.satisfies("E[q U p]"))
-    print(mc_until.satisfies("A[q U p]"))
+    queries = [
+        ("EF p", mc.satisfies("EF p")),
+        ("AG p", mc.satisfies("AG p")),
+        ("AF p", mc.satisfies("AF p")),
+        ("EG q", mc.satisfies("EG q")),
+        ("E[q U p]", mc_until.satisfies("E[q U p]")),
+        ("A[q U p]", mc_until.satisfies("A[q U p]")),
+    ]
+
+    print("Results of example CTL queries:")
+    for formula, result in queries:
+        print(f"{formula}: {result}")
 
 
 if __name__ == "__main__":
